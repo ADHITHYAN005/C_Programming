fce423b9e96384bfc32aab1205ef593a3d5e9136
ADHITHYAN005 <adhithyankrr05@gmail.com>
2026-06-02 13:11:09 +0530

    Create C_Problem_25.c

---
+++ C_Assessment1/C_Problem_25.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num, sum;
+    printf("Enter a 3-digit number: ");
+    scanf("%d", &num);
+    sum = (num / 100) + (num / 10) % 10 + (num % 10);
+    while (sum >= 10)
+    {
+        sum = (sum / 10) + (sum % 10);
+    }
+    printf("Result = %d", sum);
+    return 0;
+}
