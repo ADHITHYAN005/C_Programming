957ea1c4618409cf6396ec9e9dba4d2f97243399
ADHITHYAN005 <adhithyankrr05@gmail.com>
2026-06-02 11:04:43 +0530

    Add C program to extract digits from a two-digit number

---
+++ C_Assessment1/C_Problem_6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter a  two digit number:");
+	scanf("%d",&a);
+	printf("ones digit = %d", a / 10);
+	printf("tens digit = %d", a % 10);
+	return 0;
+	
+}
