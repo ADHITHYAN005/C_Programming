f614b4c45f27828577faeefde3ffff0e40702401
ADHITHYAN005 <adhithyankrr05@gmail.com>
2026-06-03 16:27:13 +0530

    Create C_Problem_5.c

---
+++ C_Assessment3/C_Problem_5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter a number:");
+	scanf("%d", &a);
+	
+	if(a<=50)
+	{
+		printf("success\n");
+	}
+	else
+	{
+		printf("failure\n");
+	}
+	return 0;
+}
+	
+	
