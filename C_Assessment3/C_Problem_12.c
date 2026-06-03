d578579b876c47fd89728bf432784c90a8884171
ADHITHYAN005 <adhithyankrr05@gmail.com>
2026-06-03 16:39:45 +0530

    Create C_Problem_12.c

---
+++ C_Assessment3/C_Problem_12.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,x,y;
+	printf("enter a number:");
+	scanf("%d", &a);
+	x = a/10;
+	y = a%10;
+	if(x>=y)
+	{
+		printf("success\n");
+	}
+	else
+	{
+	printf("failure\n");	
+	}
+	return 0;
+}
+	
