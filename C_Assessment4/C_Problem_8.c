26943f15bab048873b4e4756589d2202dca1466b
ADHITHYAN005 <adhithyankrr05@gmail.com>
2026-06-04 20:52:33 +0530

    Add C program to print specific odd numbers

---
+++ C_Assessment4/C_Problem_8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void printOdd(int n)
+{
+    int tens, ones;
+
+    if (n > 99)
+        return;
+
+    tens = n / 10;
+    ones = n % 10;
+
+    if (n % 2 == 0 && (tens + ones == 6))
+        printf("%d ", n);
+
+    printOdd(n + 1);
+}
+
+int main()
+{
+    printOdd(10);
+    return 0;
+}
